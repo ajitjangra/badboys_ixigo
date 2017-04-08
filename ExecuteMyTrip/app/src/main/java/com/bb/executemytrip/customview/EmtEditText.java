2a7fbb5588a80374647d9175eed4e6a9f012b3d0
Ajit Jangra <ajit.jangra@goibibo.com>
2017-04-08 13:06:36 +0530

    Font added

---
+++ ExecuteMyTrip/app/src/main/java/com/bb/executemytrip/customview/EmtEditText.java
@@ -0,0 +1,54 @@
+package com.bb.executemytrip.customview;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.bb.executemytrip.R;
+
+
+public class EmtEditText extends EditText {
+  public EmtEditText(final Context context) {
+    super(context);
+    if (!isInEditMode()) {
+      init();
+    }
+  }
+
+  public EmtEditText(final Context context, final AttributeSet attrs) {
+    super(context, attrs);
+    if (!isInEditMode()) {
+      init();
+    }
+  }
+
+  public EmtEditText(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    if (!isInEditMode()) {
+      init();
+    }
+  }
+
+  private void init() {
+    Typeface tf = null;
+
+    if (getTag() != null) {
+      if (getTag().equals(getResources().getString(R.string.font_light))) {
+        tf = Typeface.createFromAsset(getContext().getAssets(), "font/Roboto-Light.ttf");
+      } else if (getTag().equals(getResources().getString(R.string.font_medium))) {
+        tf = Typeface.createFromAsset(getContext().getAssets(), "font/Roboto-Medium.ttf");
+      } else if (getTag().equals(getResources().getString(R.string.font_bold))) {
+        tf = Typeface.createFromAsset(getContext().getAssets(), "font/Roboto-Bold.ttf");
+      } else if (getTag().equals(getResources().getString(R.string.font_regular))) {
+        tf = Typeface.createFromAsset(getContext().getAssets(), "font/Roboto-Regular.ttf");
+      }
+    }
+
+    if (tf == null) {
+      tf = Typeface.createFromAsset(getContext().getAssets(), "font/Roboto-Regular.ttf");
+    }
+
+    setTypeface(tf);
+  }
+}
