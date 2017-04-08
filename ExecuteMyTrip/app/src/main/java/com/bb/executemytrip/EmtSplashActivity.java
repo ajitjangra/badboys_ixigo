16093787a5f20a66a5ba96de4e07d0daef564189
Ajit Jangra <ajit.jangra@goibibo.com>
2017-04-08 16:56:14 +0530

    Update

--- ExecuteMyTrip/app/src/main/java/com/bb/executemytrip/EmtSplashActivity.java
+++ ExecuteMyTrip/app/src/main/java/com/bb/executemytrip/EmtSplashActivity.java
@@ -1,6 +1,8 @@
 package com.bb.executemytrip;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class EmtSplashActivity extends AppCompatActivity {
@@ -10,6 +12,12 @@ public class EmtSplashActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_emt_splash);
 
-
+    Handler handler = new Handler();
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        startActivity(new Intent(EmtSplashActivity.this, HomeActivity.class));
+      }
+    }, 500);
   }
 }
